0f191c3cd9843338d05bb53ce0ae96533683e6d9
anuraggupta9539 <sahuanurag8874@gmail.com>
2026-07-09 09:20:00 +0530

    Add files via upload

---
+++ Dem.java
@@ -0,0 +1,33 @@
+public class Dem {
+    public static void main(String[] args){
+        // Integers
+        byte a = 2;
+        // byte a = 0b10;
+        // byte a = 017;
+        // byte a = 0xB;
+
+        short b = 12;
+        int c = 120;
+        // long d =24356; 
+        long d = 24_35_62_342; 
+
+
+        // Real
+        float e = 13.43f;
+        // double f = 324.42021;
+        double f = 6.022e23;
+
+
+        // Characters
+        char g ='c';
+
+        // boolean
+        boolean h = false;
+
+        System.out.println("Integer values -->" + a + "," + b + "," + c + "," + d);
+        System.out.println("Floating values -->" + e + "," + f );
+        System.out.println("Character values -->" + g );
+        System.out.println("Boolean values -->" + h );
+
+    }
+}
---
+++ Demo.java
@@ -0,0 +1,9 @@
+package lec1;
+
+public class Demo{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+ 
